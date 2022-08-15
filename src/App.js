65775b92827593b13bd6a8ae401fe0a558b11826
Martin Rombach <martinrombach88@gmail.com>
2022-08-15 19:54:55 +0900

    fixed mobile css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //For SQL
-import { React, useState, useEffect } from "react";
+// import { React, useState, useEffect } from "react";
 
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home.js";
@@ -15,68 +15,68 @@ import ContactInquiry from "./components/Contact/ContactInquiry.js";
 import { LangProvider } from "./components/Header/LangContext";
 import "./components/SnapScroll/SnapScroll.css";
 import SubmittedForm from "./components/Contact/SubmittedForm";
-// import localWorkList from "./components/Work/localWorkList";
-// import localStaffList from "./components/localStaffList";
-// import localNewsList from "./components/localNewsList";
+import localWorkList from "./components/Work/localWorkList";
+import localStaffList from "./components/localStaffList";
+import localNewsList from "./components/localNewsList";
 
 function App() {
-  const [workObject, setWorkObject] = useState(null);
-  const [newsObject, setNewsObject] = useState(null);
-  const [staffObject, setStaffObject] = useState(null);
+  //   const [workObject, setWorkObject] = useState(null);
+  //   const [newsObject, setNewsObject] = useState(null);
+  //   const [staffObject, setStaffObject] = useState(null);
 
-  useEffect(() => {
-    const fetchNewsData = () => {
-      fetch("http://localhost:8080/getnews")
-        .then((response) => response.json())
-        .then((result) => {
-          setNewsObject(result);
-        })
-        .catch((error) => console.log("An error occurred"));
-    };
+  //   useEffect(() => {
+  //     const fetchNewsData = () => {
+  //       fetch("http://localhost:8080/getnews")
+  //         .then((response) => response.json())
+  //         .then((result) => {
+  //           setNewsObject(result);
+  //         })
+  //         .catch((error) => console.log("An error occurred"));
+  //     };
 
-    const fetchStaffData = () => {
-      fetch("http://localhost:8080/getstaff")
-        .then((response) => response.json())
-        .then((result) => setStaffObject(result))
-        .catch((error) => console.log("An error occurred"));
-    };
+  //     const fetchStaffData = () => {
+  //       fetch("http://localhost:8080/getstaff")
+  //         .then((response) => response.json())
+  //         .then((result) => setStaffObject(result))
+  //         .catch((error) => console.log("An error occurred"));
+  //     };
 
-    const fetchWorkData = () => {
-      fetch("http://localhost:8080/getwork")
-        .then((response) => response.json())
-        .then((result) => setWorkObject(result))
-        .catch((error) => console.log("An error occurred"));
-    };
-    fetchNewsData();
-    fetchStaffData();
-    fetchWorkData();
-  }, []);
+  //     const fetchWorkData = () => {
+  //       fetch("http://localhost:8080/getwork")
+  //         .then((response) => response.json())
+  //         .then((result) => setWorkObject(result))
+  //         .catch((error) => console.log("An error occurred"));
+  //     };
+  //     fetchNewsData();
+  //     fetchStaffData();
+  //     fetchWorkData();
+  //   }, []);
 
-  let workList = workObject ? workObject.workItems : null;
-  let newsList = [];
-  if (newsObject) {
-    for (let item of newsObject.newsItems) {
-      let newItem = {
-        id: item.id,
-        bodyKR: item.bodyKR.split("~"),
-        bodyENG: item.bodyENG.split("~"),
-        dateENG: item.dateENG,
-        dateKR: item.dateKR,
-        image: item.image,
-        imageLarge: item.imageLarge,
-        titleENG: item.titleENG,
-        titleKR: item.titleKR,
-      };
-      newsList.unshift(newItem);
-    }
-  } else {
-    newsList = null;
-  }
-  let staffList = staffObject ? staffObject.staffItems : null;
+  //   let workList = workObject ? workObject.workItems : null;
+  //   let newsList = [];
+  //   if (newsObject) {
+  //     for (let item of newsObject.newsItems) {
+  //       let newItem = {
+  //         id: item.id,
+  //         bodyKR: item.bodyKR.split("~"),
+  //         bodyENG: item.bodyENG.split("~"),
+  //         dateENG: item.dateENG,
+  //         dateKR: item.dateKR,
+  //         image: item.image,
+  //         imageLarge: item.imageLarge,
+  //         titleENG: item.titleENG,
+  //         titleKR: item.titleKR,
+  //       };
+  //       newsList.unshift(newItem);
+  //     }
+  //   } else {
+  //     newsList = null;
+  //   }
+  //   let staffList = staffObject ? staffObject.staffItems : null;
 
-  // let workList = localWorkList;
-  // let newsList = localNewsList;
-  // let staffList = localStaffList;
+  let workList = localWorkList;
+  let newsList = localNewsList;
+  let staffList = localStaffList;
 
   return (
     <LangProvider>
