c1df089158dee345e2ce60782c18ae68ede1738d
Martin Rombach <martinrombach88@gmail.com>
2023-10-28 20:53:49 +0900

    fixes on staff

--- src/components/About/AboutStaffName.js
+++ src/components/About/AboutStaffName.js
@@ -4,33 +4,18 @@ import "./About.css";
 const AboutStaffName = ({ name, setProfile }) => {
   const lang = useLang();
 
-  const infoListKR = <ul>
-{name.infoKR.map((item) => <li>{item}</li>)}
-  </ul>
-
-  const infoListENG = <ul>
-  {name.infoENG.map((item) => <li>{item}</li>)}
-    </ul>
-
-  const fullProfileKR = (
+  const fullProfile =
     <div>
-      <h2 className="aboutStaff__ContentHeader">{name.nameKR}</h2>
-      <h2>{name.titleKR}</h2>
-      {infoListKR}
+      <h2 className="aboutStaff__ContentHeader">{lang ? name?.nameKR : name?.nameENG}</h2>
+      <h2>{lang ? name.titleKR : name.titleENG}</h2>
+      <ul>{lang ? name.infoKR.map((item) => <li>{item}</li>) :  name.infoENG.map((item) => <li>{item}</li>)}</ul>
     </div>
-  );
-  const fullProfileENG = (
-    <div>
-      <h2 className="aboutStaff__ContentHeader">{name.nameENG}</h2>
-      <h2>{name.titleENG}</h2>
-      {infoListENG}
-    </div>
-  );
+  
 
   return (
     <div
       className="aboutStaff__Name"
-      onClick={() => setProfile(lang ? fullProfileKR : fullProfileENG)}
+      onClick={() => setProfile(fullProfile)}
     >
       <h5>{lang ? name.nameKR : name.nameENG}</h5>
       <h6>{lang ? name.titleKR : name.titleENG}</h6>
